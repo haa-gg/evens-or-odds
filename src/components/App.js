a4fc5db6fe9a5b6ed11dc6f975736a221ada0e88
Glendon Gengel <glendon.gengel@gmail.com>
2022-12-12 17:48:35 -0600

    updates

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-
+import {connect} from 'react-redux';
 
 class App extends Component {
     state = {displayBio:false};
--- src/index.js
+++ src/index.js
@@ -1,72 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {configureStore} from '@reduxjs/toolkit';
+import {Provider} from 'react-redux';
+import rootReducer from './reducers';
 import App from './components/App';
 import './index.css';
-
-const DEFAULT_SETTINGS = {
-    gameStarted: false,
-    instructionsExpanded: false,
-};
-
-const SET_GAME_STARTED = 'SET_GAME_STARTED';
-const SET_INSTRUCTIONS_EXPANDED = 'SET_INSTRUCTIONS_EXPANDED';
-
-const rootReducer = (state = DEFAULT_SETTINGS, action) => {
-    console.log('state', state, 'action', action);
-
-    if (action.type === SET_GAME_STARTED ){
-        return {
-            gameStarted: action.gameStarted,
-            instructionsExpanded: false
-        }
-    }
-
-    return state;
-};
 
 const store = configureStore({
     reducer: rootReducer
 });
-
-console.log('store', store);
 store.subscribe(() => {
     console.log('store.getState()', store.getState() );
 });
 
-const startGame = () => {
-    return{
-        type: SET_GAME_STARTED,
-        gameStarted: true
-    };
-}
-
-const cancelGame = () => {
-    return{
-        type: SET_GAME_STARTED,
-        gameStarted: false
-    };
-}
-
-const expandInstructions = () => {
-    return{
-        type: SET_INSTRUCTIONS_EXPANDED,
-        gameStarted: true
-    };
-}
-
-const collapseInstructions = () => {
-    return{
-        type: SET_INSTRUCTIONS_EXPANDED,
-        gameStarted: false
-    };
-}
-
-
-store.dispatch(startGame());
-store.dispatch(cancelGame());
-store.dispatch(expandInstructions());
-store.dispatch(collapseInstructions());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />)
+root.render(
+    <Provider store={store}>
+<App />
+</Provider>
+)
