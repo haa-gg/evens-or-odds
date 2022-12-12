edc301410de67178379a96cca48e9a5c60c4b020
Glendon Gengel <glendon.gengel@gmail.com>
2022-12-12 16:02:33 -0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,17 +9,20 @@ const DEFAULT_SETTINGS = {
     instructionsExpanded: false,
 };
 
-const rootReducer = (state, action) => {
+const SET_GAME_STARTED = 'SET_GAME_STARTED';
+const SET_INSTRUCTIONS_EXPANDED = 'SET_INSTRUCTIONS_EXPANDED';
+
+const rootReducer = (state = DEFAULT_SETTINGS, action) => {
     console.log('state', state, 'action', action);
 
-    if (action.type === 'SET_GAME_STARTED'){
+    if (action.type === SET_GAME_STARTED ){
         return {
             gameStarted: action.gameStarted,
             instructionsExpanded: false
         }
     }
 
-    return DEFAULT_SETTINGS;
+    return state;
 };
 
 const store = configureStore({
@@ -29,16 +32,41 @@ const store = configureStore({
 console.log('store', store);
 store.subscribe(() => {
     console.log('store.getState()', store.getState() );
-})
+});
 
-const action1 = {type: 'SET_GAME_STARTED', gameStarted: true};
+const startGame = () => {
+    return{
+        type: SET_GAME_STARTED,
+        gameStarted: true
+    };
+}
 
-store.dispatch(action1);
+const cancelGame = () => {
+    return{
+        type: SET_GAME_STARTED,
+        gameStarted: false
+    };
+}
+
+const expandInstructions = () => {
+    return{
+        type: SET_INSTRUCTIONS_EXPANDED,
+        gameStarted: true
+    };
+}
+
+const collapseInstructions = () => {
+    return{
+        type: SET_INSTRUCTIONS_EXPANDED,
+        gameStarted: false
+    };
+}
 
 
-
-store.dispatch({type: 'foo'});
-store.dispatch({type: 'bar'});
+store.dispatch(startGame());
+store.dispatch(cancelGame());
+store.dispatch(expandInstructions());
+store.dispatch(collapseInstructions());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />)
